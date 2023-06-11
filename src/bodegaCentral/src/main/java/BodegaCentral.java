adb94e653c53434ebe2e062e079de7d190673044
alejandroTTT <alejandro@twototango.co>
2023-06-11 21:58:45 +0000

    Refactor: Folder Structure :bug:

--- src/bodegaCentral/src/main/java/BodegaCentral.java
+++
@@ -1,6 +0,0 @@
-public class BodegaCentral {
-
-    public static void main(String[] args) {
-        System.out.println("Sin Implementación");
-    }
-}
--- src/coffeeMach/src/main/java/McControlador/VentaService.java
+++
@@ -1,5 +0,0 @@
-package McControlador;
-
-public class VentaService {
-
-}

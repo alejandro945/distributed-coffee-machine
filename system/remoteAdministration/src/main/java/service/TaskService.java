0e5e4b4c8d7ba742e89079e41d550e15a0cc0e2f
alejandroTTT <alejandro@twototango.co>
2023-06-14 02:05:54 +0000

    Props

--- system/remoteAdministration/src/main/java/service/TaskService.java
+++ system/remoteAdministration/src/main/java/service/TaskService.java
@@ -1,6 +1,7 @@
 package service;
 
 import java.util.Date;
+import java.util.concurrent.Semaphore;
 
 import model.OrdenEntrega;
 import model.OrdenTrabajo;
@@ -25,17 +26,19 @@ public class TaskService implements Runnable {
     private CallbackPrx cb;
     private int type;
     private int idMaq;
-    private String description; 
+    private String description;
     private Moneda moneda;
+    private Semaphore sem;
 
     public TaskService(OrdenManager ordenManager, AlarmasManager manager, MessageBrokerPrx messageBroker,
-            CallbackPrx cb, int type, int idMaq) {
+            CallbackPrx cb, int type, int idMaq, Semaphore sem) {
         this.ordenManager = ordenManager;
         this.manager = manager;
         this.messageBroker = messageBroker;
         this.cb = cb;
         this.type = type;
         this.idMaq = idMaq;
+        this.sem = sem;
     }
 
     public void setMoneda(Moneda moneda) {
@@ -51,24 +54,38 @@ public class TaskService implements Runnable {
      */
     @Override
     public void run() {
-        switch (type) {
-            case 1:
-                recibirNotificacionEscasezIngredientes(this.description);
-                break;
-            case 2:
-                recibirNotificacionInsuficienciaMoneda(this.moneda);
-                break;
-            case 3:
-                recibirNotificacionEscasezSuministro();
-                break;
-            case 4:
-                recibirNotificacionMalFuncionamiento();
-                break;
-            case 5:
-                recibirNotificacionAbastesimiento();
-                break;
-            default:
-                break;
+        try {
+            switch (type) {
+                case 1:
+                    sem.acquire();
+                    recibirNotificacionEscasezIngredientes(this.description);
+                    sem.release();
+                    break;
+                case 2:
+                    sem.acquire();
+                    recibirNotificacionInsuficienciaMoneda(this.moneda);
+                    sem.release();
+                    break;
+                case 3:
+                    sem.acquire();
+                    recibirNotificacionEscasezSuministro();
+                    sem.release();
+                    break;
+                case 4:
+                    sem.acquire();
+                    recibirNotificacionMalFuncionamiento();
+                    sem.release();
+                    break;
+                case 5:
+                    sem.acquire();
+                    recibirNotificacionAbastesimiento();
+                    sem.release();
+                    break;
+                default:
+                    break;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
@@ -102,19 +119,19 @@ public class TaskService implements Runnable {
         feedback(alarmamoneda, idMaq, alarmId, response, messageBroker, cb);
     }
 
-    private void recibirNotificacionEscasezSuministro(){
+    private void recibirNotificacionEscasezSuministro() {
         int alarmId = manager.alarmaMaquina(ALARMA_SUMINISTRO, idMaq, new Date());
         int[] response = createOrdenes(idMaq, alarmId, new int[] { 0, 1, 0 });
         feedback(ALARMA_SUMINISTRO, idMaq, alarmId, response, messageBroker, cb);
     }
 
-    private void recibirNotificacionMalFuncionamiento(){
+    private void recibirNotificacionMalFuncionamiento() {
         int alarmId = manager.alarmaMaquina(ALARMA_MAL_FUNCIONAMIENTO, idMaq, new Date());
         int[] response = createOrdenes(idMaq, alarmId, new int[] { 1, 0, 0 });
         feedback(ALARMA_MAL_FUNCIONAMIENTO, idMaq, alarmId, response, messageBroker, cb);
     }
 
-    private void recibirNotificacionAbastesimiento(){
+    private void recibirNotificacionAbastesimiento() {
         manager.desactivarAlarma(ALARMA_INGREDIENTE, idMaq, new Date());
         feedback(ALARMA_INGREDIENTE, idMaq, 0, new int[] {}, messageBroker, cb);
     }

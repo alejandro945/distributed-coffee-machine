5d55faac28880edde62c95903f97a83e463f2cfb
alejandroTTT <alejandro@twototango.co>
2023-06-14 21:11:36 +0000

    Props

--- system/inventory/src/main/java/services/OrdenEntregaImp.java
+++ system/inventory/src/main/java/services/OrdenEntregaImp.java
@@ -20,7 +20,7 @@ public class OrdenEntregaImp implements OrdenBodega {
     public boolean atenderOperador(int codOp, int codOrden, Current current) {
         // Se atiende al operador actualizar orden de entrega con el id del operador
         System.out.println("Ha llegado el operador a la bodega");
-        controladorBodega.updateOrden(codOrden, codOp);
+       /*  controladorBodega.updateOrden(codOrden, codOp); */
         System.out.println("Se ha actualizado correctamente el operador de la orden de entrega");
         // Al final se hace un feedback a logistica Aknowledgement
        // ordenLogistica.confirmarOrden(codOrden);
@@ -31,7 +31,7 @@ public class OrdenEntregaImp implements OrdenBodega {
     public void notificarOrdenEntrega(int code,Current current) {
         System.out.println("Ha llegado una notificacion de alarma de orden de entrega de alarma" + code);
         // Llega la alama desde el servidor central
-        controladorBodega.confirmarOrden(code);
+        /* controladorBodega.confirmarOrden(code); */
     }
 
     
